8bf58c795b1dba1fa4941e9882d3a39dcf7b277c
EstebanXP <a324249@uach.mx>
2021-10-03 20:51:53 -0600

    0.3 bdd y ejemplo

---
+++ database/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBLbUC8BT6f-ZJF3WLTwN6WbJajcca28LE",
+    authDomain: "abbey-road-app.firebaseapp.com",
+    projectId: "abbey-road-app",
+    storageBucket: "abbey-road-app.appspot.com",
+    messagingSenderId: "126323543490",
+    appId: "1:126323543490:web:23c0f99d65cfb044c51a5b"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db=firebase.firestore();
+
+export default{
+    firebase,
+    db,
+}
--- screens/songsManagement.js
+++ screens/songsManagement.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import { Text, View, Button ,SafeAreaView,StyleSheet,TextInput} from 'react-native';
+import firebase from "../database/firebase";
 
 export default function songsManagement() {
+    const [state,setState]= useState([]);
+    
+    useEffect(()=>{
+      firebase.db.collection('songs').onSnapshot(querySnapshot=>{
+        querySnapshot.docs.forEach(doc=>{
+          console.log(doc.id,doc.data());
+        })
+      })
+    })
     return (
       <SafeAreaView>
+        {console.log("Hola mundo")}
           <TextInput 
             style={styles.input}
-            placeholder="Buscar cancion"
+            placeholder="Nombre de la cancion"
+          />
+          <TextInput 
+            style={styles.input}
+            placeholder="Nombre del artista"
           />
       </SafeAreaView>
     );
