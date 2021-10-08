cfac99a0694a050b4fe514953b6785279ba06985
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2021-10-08 00:26:00 -0600

    TextBoxes

--- screens/registerUser.js
+++ screens/registerUser.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 import firebase from "../database/firebase";
 
 export default function registerUser() {
     return (
       <View style={styles.container}>
         <Text>Hola mundo desde el registro</Text>
+        <TextInput 
+            style={styles.input}
+            placeholder="Username"
+          />
+          <TextInput 
+            style={styles.input}
+            placeholder="Password"
+          />
+          <TextInput 
+            style={styles.input}
+            placeholder="Name"
+          />
+          <TextInput 
+            style={styles.input}
+            placeholder="Rol"
+          />
       </View>
     );
 }
@@ -16,6 +32,12 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
     },
   });
   
