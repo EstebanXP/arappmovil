b3a9103540a01ca98511aada9a8ecbddc44c2a0c
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2021-10-08 20:01:51 -0600

    heyo

--- screens/loginUser.js
+++ screens/loginUser.js
@@ -1,41 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View,TextInput } from 'react-native';
+import { Button } from "react-native-elements/dist/buttons/Button";
 import firebase from "../database/firebase";
 
 export default function registerUser() {
-    function componentDidMoun(){
-        this.checkIfLoggedIn();
-    }
-
-    //Checa si el usuario esta Loggeado, falta modificar las vistas
-    checkIfLoggedIn = () => {
-        firebase.auth().onAuthStateChanged(user=>{
-            if(user){
-                this.props.navigation.navigate('Login User');
-            }
-            else{
-                this.props.navigation.navigate('Register User');
-            }
-        })
-    }
-    /*useEffect(()=>{
-        firebase.db.collection('songs').onSnapshot(querySnapshot=>{
-          querySnapshot.docs.forEach(doc=>{
-            console.log(doc.id,doc.data());
-          })
-        })
-      },[])*/
     return (
       <View style={styles.container}>
-        <Text>Hola mundo desde el registro</Text>
-        <TextInput 
-            style={styles.input}
-            placeholder="Username"
-          />
-          <TextInput 
-            style={styles.input}
-            placeholder="Password"
-          />
+        <Text>Inicia Sesion</Text>
+        <Button title='Sign In With Google' onPress={() =>alert('button pressed')}></Button>
       </View>
     );
 }
