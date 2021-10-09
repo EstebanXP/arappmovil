e6e8cfaf015daa9626811c086efd29724e87870a
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2021-10-09 03:21:55 -0600

    cAmbiOs eN lOgIn

--- screens/loginUser.js
+++ screens/loginUser.js
@@ -4,6 +4,11 @@ import { Button } from "react-native-elements/dist/buttons/Button";
 import firebase from "../database/firebase";
 
 export default function registerUser() {
+    const handleGoogleSignIn = () => {
+        const config = {iosClientId : `126323543490-qo8841if2ddlrss6c5hru3uspkc4elu5.apps.googleusercontent.com`, 
+                        androidClientId: `126323543490-jsbg1t6cpnvjletki8udrkn3sb341sfv.apps.googleusercontent.com`}
+    }
+
     return (
       <View style={styles.container}>
         <Text>Inicia Sesion Aqui</Text>
