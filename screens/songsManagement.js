e946ed565c0b837b987a402c7d4e3ca05b1fea5a
EstebanXP <a324249@uach.mx>
2021-10-09 21:10:46 -0600

    0.4 ya busca canciones, falta editar y borrar

--- screens/songsManagement.js
+++ screens/songsManagement.js
@@ -3,27 +3,103 @@ import { Text, View, Button ,SafeAreaView,StyleSheet,TextInput} from 'react-nati
 import firebase from "../database/firebase";
 
 export default function songsManagement() {
-    const [state,setState]= useState([]);
+    const [songs,setSongs]= useState([]);
+    const [status,setStatus]=useState(false);
+    const [lyrics,setLyrics]= useState("");
+    const [artist,setArtist]=useState("");
+    const [title,setTitle]=useState("");
+    const [song, setSong] = useState({
+      title: "",
+      artist: "",
+      chords: "",
+  })
+
+    const handleChangeText = (field, value) =>{
+      setSong({ ...song ,[field]: value});
+  }
+
+    function fetchApi(){
+      return fetch(`https://api.lyrics.ovh/v1/${song.artist}/${song.title}`);
+    }
+
+    async function saveData(){
+      await firebase.db.collection('songs').add({
+        artist: song.artist,
+        title:song.title,
+        lyrics: lyrics,
+        chords:song.chords
+        
+      })
+      .then(()=>{
+        console.log("Guardado")
+      })
+      .catch((err)=>{
+        console.log(error);
+      })
+    }
+
+    async function getSongfromAPI(){
+      try{
+        const response=await fetchApi();
+        if(response.status===200){
+          setStatus(!status);
+          const json=await response.json();
+          setLyrics(json.lyrics);
+        }
+        
+      }catch(error){
+        console.log(error);
+      }
+    }
     
     useEffect(()=>{
+       getSongfromAPI();
       firebase.db.collection('songs').onSnapshot(querySnapshot=>{
+        const docs=[];
         querySnapshot.docs.forEach(doc=>{
-          console.log(doc.id,doc.data());
-        })
+          const {artist,chords,lyrics,title}=doc.data();
+          docs.push({
+            id:doc.id,
+            artist:artist,
+            chords:chords,
+            lyrics:lyrics,
+            title:title
+          })
+        });
+        setSongs(docs);
       })
-    })
+    },[])
     return (
-      <SafeAreaView>
-        {console.log("Hola mundo")}
+      <View>
+        <SafeAreaView>
           <TextInput 
             style={styles.input}
             placeholder="Nombre de la cancion"
+            onChangeText={text=>handleChangeText("title",text)}
           />
           <TextInput 
             style={styles.input}
             placeholder="Nombre del artista"
+            onChangeText={text=>handleChangeText("artist",text)}
           />
+          <Button title="Search Song" onPress={()=>getSongfromAPI()}></Button>
+          {status===true?
+          <Button title="Save song" onPress={()=>saveData()}></Button>
+            :
+            <Text>Tu cancion se encontrara abajo!</Text>
+        }
       </SafeAreaView>
+      {status===true?
+      <View>
+        <Text>Letra Encontrada: </Text>
+        <Text>{lyrics}</Text>
+        <Button title="Save song" onPress={()=>saveData()}></Button>
+        </View>
+        :
+        <Text>Boludo</Text>
+    }
+
+      </View>
     );
 }
 
