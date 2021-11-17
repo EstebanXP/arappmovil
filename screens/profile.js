450847d1956d786376d433e6516fa525d3c7346e
Efrain Estrada <47330403+kevoest@users.noreply.github.com>
2021-11-17 01:30:55 -0700

    Autsilio erick sempai

--- screens/profile.js
+++ screens/profile.js
@@ -15,35 +15,41 @@ import {
   Center,
   Pressable,
 } from 'native-base';
-import { View} from 'react-native'
+import { View, SafeAreaView} from 'react-native'
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { NavigationContext } from '@react-navigation/native';
 import * as RootNavigation from "./RootNavigation"
 
 const Example = () => {
     return (
-        <Image
+        <SafeAreaView>
+            <Image
           size={200}
           resizeMode={"contain"}
           borderRadius={100}
-          mt="50"
+          mt="-300"
           source={{
             uri: "https://wallpaperaccess.com/full/317501.jpg",
           }}
           alt="Alternate Text"
         />
+        <Heading textAlign="center" color="black" fontSize="3xl" mt="-5">
+                Name
+            </Heading>
+            <Text textAlign="center" color="black" >lg</Text>
+        </SafeAreaView> 
       )
     }
 
     export default function profile(){
       return (
-        <NativeBaseProvider>
+        <SafeAreaView>
+             <NativeBaseProvider>
             <Center flex={1} px="9">
                 <Example />
             </Center>
-            <Text textAlign="center" color="black" fontSize="80" mb="450">
-                Name 
-            </Text>
         </NativeBaseProvider>
+        </SafeAreaView>
+       
       )
     }
