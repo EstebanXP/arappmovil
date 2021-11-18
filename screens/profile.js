aa5bc6d0a4d6630110f7d93e542fdb97e53191c3
erickdominguez <47347282+erickdominguez@users.noreply.github.com>
2021-11-18 01:36:32 -0700

    Boton de sign out

--- screens/profile.js
+++ screens/profile.js
@@ -15,6 +15,7 @@ import {
   Center,
   Pressable,
 } from 'native-base';
+import firebase from "../database/firebase";
 import { View, SafeAreaView} from 'react-native'
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { NavigationContext } from '@react-navigation/native';
@@ -41,6 +42,7 @@ import * as RootNavigation from "./RootNavigation"
                     </Heading>
                     <Text textAlign="center" color="black" position="absolute" top="72" >@UserName</Text>
                     <Text textAlign="center" color="black" position="absolute" top="80" >Role</Text>
+                    <Button  bg="danger.500" size={'lg'} colorScheme="red" onPress={firebase.firebase.app().auth().signOut()} shadow={9} px="100" mt="64" w="100%" position="absolute" top="80" borderRadius="50">Sign Out</Button>
                 </Center>
             </NativeBaseProvider>
         </View>    
