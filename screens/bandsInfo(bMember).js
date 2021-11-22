906eccbf271d0d13911ac969923b0ee573322d34
alnair gonz <a326669@uach.mx>
2021-11-22 11:45:13 -0700

    se añadieron logos

--- screens/bandsInfo(bMember).js
+++ screens/bandsInfo(bMember).js
@@ -57,7 +57,7 @@ export default function bandsInfo(props,{navigation}) {
                     top="0"
                     
                     source={{
-                        uri: "https://images.unsplash.com/photo-1598518141787-5be70e839626?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1776&q=80",
+                        uri: band.bandLogo,
                     }}
                     alt="Alternate Text"
                     />      
