fc975c003e9c207d1149903efbcdd98a63dd1bbd
alnair gonz <a326669@uach.mx>
2021-11-21 20:18:40 -0700

    Update backup.js

--- screens/backup.js
+++ screens/backup.js
@@ -20,6 +20,7 @@ import { View, SafeAreaView} from 'react-native'
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { NavigationContext } from '@react-navigation/native';
 import * as RootNavigation from "./RootNavigation"
+import { AntDesign, Entypo } from '@expo/vector-icons';
 
 
 export default function profile({route,navigation}){
@@ -35,16 +36,20 @@ export default function profile({route,navigation}){
                     </Heading>     
                 </Center>
                 <Box alignItems="center" position="absolute" top="2" w="90%" right="5%" left="5%">
-                <Image
-                    size={100}
-                    resizeMode={"contain"}
-                    borderRadius={100}
-                    position="absolute" top="150"
-                    source={{
-                        uri: "https://wallpaperaccess.com/full/317501.jpg",
-                    }}
-                    alt="Alternate Text"
-                />
+                <Icon
+                      as={<MaterialCommunityIcons name="apple-icloud" />}
+                      size={60}
+                      left="30%"
+                      position="absolute" top="150"
+                      bold
+                      />
+                <Icon
+                      as={<MaterialCommunityIcons name="dropbox" />}
+                      size={60}
+                       left="60%"
+                      position="absolute" top="150"
+                      bold
+                      />      
                 </Box>
             </NativeBaseProvider>
         </View>    
